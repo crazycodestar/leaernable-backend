1d209aecae62694d6a9e2be2261ccf4dfa114941
olalekan <olamilekanadekanmbi@gmail.com>
2022-07-23 19:02:38 +0100

    built database infrastructure: Prisma

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 require("dotenv").config();
 
 const PORT = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
